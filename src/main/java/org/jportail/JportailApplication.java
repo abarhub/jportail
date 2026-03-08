0893b48be14fd2e772f88676c4a6d93c738c2451
abarhub <abarhub@gmail.com>
2026-03-08 13:27:10 +0100

    première version du projet

---
+++ src/main/java/org/jportail/JportailApplication.java
@@ -0,0 +1,13 @@
+package org.jportail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JportailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JportailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jportail/JportailApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jportail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JportailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
